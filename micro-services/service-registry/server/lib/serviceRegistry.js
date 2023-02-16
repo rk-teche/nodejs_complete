a651716a4f59ee5e535c23ee41b66c9c257df826
Rahul Kr. Porwal <porwalcs019@gmail.com>
2023-02-16 16:14:22 +0000

    micro services architecture added

---
+++ micro-services/service-registry/server/lib/serviceRegistry.js
@@ -0,0 +1,54 @@
+const semver = require('semver'); // The semantic versioner for npm
+
+class ServiceRegistry {
+    constructor(log){
+        this.log = log
+        this.services = {} // this object will have all the services
+        this.timeout = 10 // it will expire services that haven't been seen for more than 30 seconds
+    }
+
+    register(name, version, ip, port){
+        this.cleanup()
+        const key = `${name}${version}${ip}${port}`
+
+        if(!this.services[key]){
+            this.services[key] = {};
+            this.services[key].timestamp = Math.floor(new Date() / 1000) // this give us a unique time stamp
+            this.services[key].ip = ip
+            this.services[key].port = port
+            this.services[key].name = name
+            this.services[key].version = version
+
+            this.log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`)
+            
+            return key;
+        }
+        this.services[key].timestamp = Math.floor(new Date() / 1000) // updated time stamp
+        this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`)
+    }
+
+    get(name, version){
+        this.cleanup()
+        const candidates = Object.values(this.services).filter(service => service.name === name && semver.satisfies(service.version, version))
+
+        return candidates[Math.floor(Math.random()* candidates.length)] // return randam index
+    }
+
+    unregister(name, version, ip, port){
+        const key = `${name}${version}${ip}${port}`
+        delete this.services[key]
+        return key
+    }
+
+    cleanup(){
+        const now = Math.floor(new Date() / 1000)
+        Object.keys(this.services).forEach(key => {
+            if(this.services[key].timestamp + this.timeout < now){
+                delete this.services[key]
+                this.log.debug(`Removed services ${key}`)
+            }
+        })
+    }
+}
+
+module.exports = ServiceRegistry
---
+++ micro-services/service-registry/server/service.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const ServiceRegistry = require('./lib/serviceRegistry');
+
+const service = express();
+
+
+module.exports = (config) => {
+  const log = config.log();
+  const serviceRegistry = new ServiceRegistry(log)
+  // Add a request logging middleware in development mode
+  if (service.get('env') === 'development') {
+    service.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  service.put('/register/:servicename/:serviceversion/:servicereport', (req, res, next) => {
+    const {servicename, serviceversion, servicereport} = req.params
+
+    const serviceIp = req.socket.remoteAddress.includes('::') ? `[${req.socket.remoteAddress}]` : req.socket.remoteAddress
+
+    const serviceKey = serviceRegistry.register(servicename, serviceversion, serviceIp, servicereport)
+    return res.json({result: serviceKey})
+  })
+
+  service.delete('/register/:servicename/:serviceversion/:servicereport', (req, res, next) => {
+    const {servicename, serviceversion, servicereport} = req.params
+
+    const serviceIp = req.socket.remoteAddress.includes('::') ? `[${req.socket.remoteAddress}]` : req.socket.remoteAddress
+
+    const serviceKey = serviceRegistry.unregister(servicename, serviceversion, serviceIp, servicereport)
+    return res.json({result: serviceKey})
+  })
+
+  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
+    const {servicename, serviceversion} = req.params
+
+    const svc = serviceRegistry.get(servicename, serviceversion)
+    if(!svc) return res.status(400).json({result: 'Service not found'})
+
+    return res.json(svc)
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return service;
+};
